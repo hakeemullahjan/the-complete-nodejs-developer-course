cdf0e6979463d7444e58d632658b6c4545090a7c
hakeemullahjan <h.jan210@gmail.com>
2020-02-11 03:22:56 +0500

    032 Customizing HTTP Requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,8 @@
 const request = require("request")
 
-const url = 'https://api.darksky.net/forecast/4a7b9acbff1ca79ad039b3b85d79c4e8/37.8267,-122.4233'
+const url = 'https://api.darksky.net/forecast/4a7b9acbff1ca79ad039b3b85d79c4e8/37.8267,-122.4233?lang=es'
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.currently)
+request({ url: url, json: true }, (error, response) => {
+    // console.log(response.body.currently)
+    console.log('It is currently', response.body.daily.data[0].summary, 'There is', response.body.currently.precipIntensity, '% chances of rain')
 })
