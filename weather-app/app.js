e24ee53c29d2d383c2ba9f280778ca59897cfbad
hakeemullahjan <h.jan210@gmail.com>
2020-02-14 00:34:30 +0500

    033 An HTTP Request Challenge

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,5 +4,11 @@ const url = 'https://api.darksky.net/forecast/4a7b9acbff1ca79ad039b3b85d79c4e8/3
 
 request({ url: url, json: true }, (error, response) => {
     // console.log(response.body.currently)
-    console.log('It is currently', response.body.daily.data[0].summary, 'There is', response.body.currently.precipIntensity, '% chances of rain')
+    //console.log('It is currently', response.body.daily.data[0].summary, 'There is', response.body.currently.precipIntensity, '% chances of rain')
+})
+
+const URL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiaGFrZWVtdWxsYWhqYW4iLCJhIjoiY2s2bDRjbXU4MGFlazNkcDZ6bTNnZXZiZiJ9.v_IhhjsLHq_CSFd8qDe52A&limit=1'
+
+request({ url: URL, json: true }, (error, response) => {
+    console.log('lat', response.body.features[0].center[0], 'lan', response.body.features[0].center[1])
 })
