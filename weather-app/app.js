e7450dfe92d5b096bf277c7f52899d9e288bad44
hakeemullahjan <h.jan210@gmail.com>
2020-02-27 23:36:37 +0500

    036 Callback Abstraction

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,6 +1,7 @@
 const request = require("request")
+const geocode = require("./utils/geocode")
 
-const url = 'https://api.darksky.net/forecast/4a7b9acbff1ca79ad039b3b85d79c4e8/37.8267,-122.4233'
+// const url = 'https://api.darksky.net/forecast/4a7b9acbff1ca79ad039b3b85d79c4e8/37.8267,-122.4233'
 // request({ url: url, json: true }, (error, response) => {
 //     if (error) {
 //         console.log("Cant connect to weather services")
@@ -11,14 +12,19 @@ const url = 'https://api.darksky.net/forecast/4a7b9acbff1ca79ad039b3b85d79c4e8/3
 //     }
 // })
 
-const URL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiaGFrZWVtdWxsYWhqYW4iLCJhIjoiY2s2bDRjbXU4MGFlazNkcDZ6bTNnZXZiZiJ9.v_IhhjsLHq_CSFd8qDe52A&limit=1'
-request({ url: URL, json: true }, (error, response) => {
-    if (error) {
-        console.log("Cannot find geo location")
-    } else if (response.body.features.length === 0) {
-        console.log("Invalid api Location")
-    }
-    else {
-        console.log('lat', response.body.features[0].center[0], 'lan', response.body.features[0].center[1])
-    }
+// const URL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiaGFrZWVtdWxsYWhqYW4iLCJhIjoiY2s2bDRjbXU4MGFlazNkcDZ6bTNnZXZiZiJ9.v_IhhjsLHq_CSFd8qDe52A&limit=1'
+// request({ url: URL, json: true }, (error, response) => {
+//     if (error) {
+//         console.log("Cannot find geo location")
+//     } else if (response.body.features.length === 0) {
+//         console.log("Invalid api Location")
+//     }
+//     else {
+//         console.log('lat', response.body.features[0].center[0], 'lan', response.body.features[0].center[1])
+//     }
+// })
+
+geocode("karachi", (error, data) => {
+    console.log("Error", error)
+    console.log("Data", data)
 })
