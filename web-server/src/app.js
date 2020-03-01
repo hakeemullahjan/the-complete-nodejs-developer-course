7646ddbda3e86a1e37714305c42ec4be6badd39c
hakeemullahjan <h.jan210@gmail.com>
2020-03-01 11:27:37 +0500

    045 Serving up Static Assets

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,22 +1,29 @@
 const express = require("express")
 const app = express()
+const path = require("path")
 
-app.get('/', (req, res) => {
-    res.send("<h1>Weather App</h1>")
-})
+console.log(__dirname)
+console.log(path.join(__dirname, '../public'))
 
-app.get("/help", (req, res) => {
-    res.send(
-        [
-            { name: "Hakeemullah" },
-            { name: "yousufzai" }
-        ]
-    )
-})
+const publicDirectoryPath = path.join(__dirname, '../public')
+app.use(express.static(publicDirectoryPath))
 
-app.get("/about", (req, res) => {
-    res.send("<h1>About Page</h1>")
-})
+// app.get('/', (req, res) => {
+//     res.send("<h1>Weather App</h1>")
+// })
+
+// app.get("/help", (req, res) => {
+//     res.send(
+//         [
+//             { name: "Hakeemullah" },
+//             { name: "yousufzai" }
+//         ]
+//     )
+// })
+
+// app.get("/about", (req, res) => {
+//     res.send("<h1>About Page</h1>")
+// })
 
 app.get("/weather", (req, res) => {
     res.send({
