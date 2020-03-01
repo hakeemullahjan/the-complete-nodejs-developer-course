d1c1eb9bcab23c8bf12e04d3e421ec420789aaba
hakeemullahjan <h.jan210@gmail.com>
2020-03-01 11:01:56 +0500

    044 Serving up HTML and JSON

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,19 +2,27 @@ const express = require("express")
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send("Hello Express!")
+    res.send("<h1>Weather App</h1>")
 })
 
 app.get("/help", (req, res) => {
-    res.send("Help page")
+    res.send(
+        [
+            { name: "Hakeemullah" },
+            { name: "yousufzai" }
+        ]
+    )
 })
 
 app.get("/about", (req, res) => {
-    res.send("About Page!")
+    res.send("<h1>About Page</h1>")
 })
 
 app.get("/weather", (req, res) => {
-    res.send("SHow weather")
+    res.send({
+        forecast: "23 degree",
+        location: "karachi"
+    })
 })
 
 app.listen(3000, () => {
