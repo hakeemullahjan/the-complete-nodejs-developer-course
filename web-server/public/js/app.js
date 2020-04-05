1fa0e370fe4234876377654bea3a5798fbf4afcb
hakeemullahjan <h.jan210@gmail.com>
2020-04-05 15:09:29 +0500

    058 Creating a Search Form

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,20 +1,38 @@
 console.log("Client side javascript file is loaded!")
 
-fetch('http://puzzle.mead.io/puzzle').then(r => {
-    r.json().then(d => {
-        console.log(d)
-    })
-})
+// fetch('http://puzzle.mead.io/puzzle').then(r => {
+//     r.json().then(d => {
+//         console.log(d)
+//     })
+// })
 
-fetch(`http://localhost:3000/weather?address=boston`).then(r => {
-    r.json().then(data => {
-        if (data.error) {
-            console.log(data.error)
-        } else {
-            console.log(data.location)
-            console.log(data.forecast)
-        }
-    })
-}).catch(e => {
-    console.log(e)
+// fetch(`http://localhost:3000/weather?address=boston`).then(r => {
+//     r.json().then(data => {
+//         if (data.error) {
+//             console.log(data.error)
+//         } else {
+//             console.log(data.location)
+//             console.log(data.forecast)
+//         }
+//     })
+// }).catch(e => {
+//     console.log(e)
+// })
+
+const weatherForm = document.querySelector("form")
+const search = document.querySelector("input")
+weatherForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const location = search.value
+    fetch(`http://localhost:3000/weather?address=${location}`)
+        .then(r => {
+            r.json().then(data => {
+                if (data.error) {
+                    console.log(data.error)
+                } else {
+                    console.log(data.location)
+                    console.log(data.forecast)
+                }
+            })
+        })
 })
