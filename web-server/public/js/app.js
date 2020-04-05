a2deace63bdd8a710896e040566449f1ac4499f1
hakeemullahjan <h.jan210@gmail.com>
2020-04-05 14:37:47 +0500

    057 Browser HTTP Requests with Fetch

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,20 @@
 console.log("Client side javascript file is loaded!")
+
+fetch('http://puzzle.mead.io/puzzle').then(r => {
+    r.json().then(d => {
+        console.log(d)
+    })
+})
+
+fetch(`http://localhost:3000/weather?address=boston`).then(r => {
+    r.json().then(data => {
+        if (data.error) {
+            console.log(data.error)
+        } else {
+            console.log(data.location)
+            console.log(data.forecast)
+        }
+    })
+}).catch(e => {
+    console.log(e)
+})
