430be4c218439d32e08d99e2c7334c45b9778815
hakeemullahjan <h.jan210@gmail.com>
2020-04-07 12:25:31 +0500

    078 Querying Documents

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -8,11 +8,11 @@ const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
 const id = new ObjectID()
-console.log(id)
-console.log(id.getTimestamp())
-console.log(id.id)
-console.log(id.id.length)
-console.log(id.toHexString().length)
+// console.log(id)
+// console.log(id.getTimestamp())
+// console.log(id.id)
+// console.log(id.id.length)
+// console.log(id.toHexString().length)
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
     if (error) {
@@ -66,5 +66,40 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
     //     }
     //     console.log(result.ops)
     // })
+
+    // db.collection("users").findOne({ _id: new ObjectID("5e8b24e2cab31438049dbafb") }, (error, user) => {
+    //     if (error) {
+    //         return console.log('Unable to find')
+    //     }
+    //     console.log(user)
+    // })
+
+    // db.collection("users").find({ age: 22 }).toArray((error, users) => {
+    //     if (error) {
+    //         return console.log("Unable to find")
+    //     }
+    //     console.log(users)
+    // })
+
+    // db.collection("users").find({ age: 22 }).count((error, counts) => {
+    //     if (error) {
+    //         return console.log("Unable to find")
+    //     }
+    //     console.log(counts)
+    // })
+
+    // db.collection("tasks").findOne({ _id: new ObjectID("5e8b2bbd36dfd128c47f0e24") }, (error, task) => {
+    //     if (error) {
+    //         return console.log(error)
+    //     }
+    //     console.log(task)
+    // })
+
+    db.collection('tasks').find({ completed: true }).toArray((error, tasks) => {
+        if (error) {
+            return console.log(error)
+        }
+        console.log(tasks)
+    })
 
 })
