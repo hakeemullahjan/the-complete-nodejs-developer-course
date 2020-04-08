a2d8022cb6def6523cac85bbc5520c6ee398f5a7
hakeemullahjan <h.jan210@gmail.com>
2020-04-08 22:22:03 +0500

    096 AsyncAwait Part II

--- task-manager/playground/promise-chaining-2.js
+++ task-manager/playground/promise-chaining-2.js
@@ -1,10 +1,22 @@
 require("../src/db/mongoose")
 const Task = require("../src/models/task")
 
-Task.findByIdAndDelete('5e8d82ff1229800b3437159e').then((task) => {
-    console.log(task)
-    return Task.countDocuments({ completed: false })
-}).then((result) => {
+// Task.findByIdAndDelete('5e8d82ff1229800b3437159e').then((task) => {
+//     console.log(task)
+//     return Task.countDocuments({ completed: false })
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndDelete(id)
+    const incompleteTasks = await Task.countDocuments({ completed: true })
+    return incompleteTasks
+}
+
+deleteTaskAndCount("5e8dbafdbb3e0b24cc4cc102").then((result) => {
     console.log(result)
 }).catch((e) => {
     console.log(e)
--- task-manager/playground/promise-chaining.js
+++ task-manager/playground/promise-chaining.js
@@ -1,11 +1,23 @@
 require('../src/db/mongoose')
 const User = require("../src/models/user")
 
-User.findByIdAndUpdate('5e8d8d7270c0cb0f20c29179', { age: 90 }).then((user) => {
-    console.log(user)
-    return User.countDocuments({ age: 90 })
-}).then((result) => {
-    console.log(result)
+// User.findByIdAndUpdate('5e8d8d7270c0cb0f20c29179', { age: 90 }).then((user) => {
+//     console.log(user)
+//     return User.countDocuments({ age: 90 })
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age });
+    const count = await User.countDocuments({ age });
+    return count
+}
+
+updateAgeAndCount("5e8d8183837fea3b18d09acd", 22).then((count) => {
+    console.log(count)
 }).catch((e) => {
     console.log(e)
 })
