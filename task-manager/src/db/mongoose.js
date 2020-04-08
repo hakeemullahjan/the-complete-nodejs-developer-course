429dd15c296e6a1a974ba505f6b8c9410cef6d16
hakeemullahjan <h.jan210@gmail.com>
2020-04-08 12:58:04 +0500

    084 Creating a Mongoose Model

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -20,9 +20,28 @@ const me = new User({
     age: 'ali'
 })
 
-me.save().then((response) => {
-    console.log(response)
-}).catch((error) => {
-    console.log(error)
+// me.save().then((response) => {
+//     console.log(response)
+// }).catch((error) => {
+//     console.log(error)
+// })
+
+const Tasks = mongoose.model("Tasks", {
+    description: {
+        type: String
+    },
+    completed: {
+        type: Boolean
+    }
 })
 
+const task = new Tasks({
+    description: "Clean house",
+    completed: 12
+})
+
+task.save().then((res) => {
+    console.log(res)
+}).catch((err) => {
+    console.log(err)
+})
