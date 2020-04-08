dc0aa51e9d7674a26ec43d471e38fae724cb47ae
hakeemullahjan <h.jan210@gmail.com>
2020-04-08 17:10:54 +0500

    091 Resource Reading Endpoints Part I

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -17,6 +17,27 @@ app.post('/users', (req, res) => {
     })
 })
 
+app.get("/users", (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((err) => {
+        res.status(500).send(err)
+    })
+})
+
+app.get("/users/:id", (req, res) => {
+    const _id = req.params.id
+
+    User.findById(_id).then((user) => {
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    }).catch((err) => {
+        res.status(500).send(err)
+    })
+})
+
 app.post('/posts', (req, res) => {
     const task = new Task(req.body)
     task.save().then((resp => {
