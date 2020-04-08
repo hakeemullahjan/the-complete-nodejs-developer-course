50b92a1cde11c57df65f6935df94e5da142c0fa6
hakeemullahjan <h.jan210@gmail.com>
2020-04-08 16:53:51 +0500

    090 Resource Creation Endpoints Part II

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 require("./db/mongoose")
 const User = require("./models/user")
+const Task = require("./models/task")
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -10,8 +11,17 @@ app.use(express.json())
 app.post('/users', (req, res) => {
     const user = new User(req.body)
     user.save().then((resp) => {
-        res.send(resp)
+        res.status(201).send(resp)
     }).catch((err) => {
+        res.status(400).send(err)
+    })
+})
+
+app.post('/posts', (req, res) => {
+    const task = new Task(req.body)
+    task.save().then((resp => {
+        res.status(201).send(resp)
+    })).catch((err) => {
         res.status(400).send(err)
     })
 })
