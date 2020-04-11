0c649ec016b76b54e641f77ea2b7b897aca83991
hakeemullahjan <h.jan210@gmail.com>
2020-04-11 13:47:44 +0500

    131 Exploring SendGrid

---
+++ task-manager/src/emails/account.js
@@ -0,0 +1,12 @@
+const sgMail = require('@sendgrid/mail')
+
+const sendgridAPIKey = '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'
+
+sgMail.setApiKey(sendgridAPIKey)
+
+sgMail.send({
+    to: 'h.jan210@gmail.com',
+    from: 'andrew@mead.io',
+    subject: 'This is my first creation!',
+    text: 'I hope this one actually get to you.'
+})
