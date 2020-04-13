bbf5ccb172e2868ee1956163efbd78452b576a0a
hakeemullahjan <h.jan210@gmail.com>
2020-04-13 17:35:53 +0500

    144 Testing with Authentication

--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -1,29 +1,29 @@
 const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit, add } = require("../src/math.js")
 
-test('should calculate total with tip', () => {
-    const total = calculateTip(10, .3)
-    expect(total).toBe(13)
-    // if (total !== 13) {
-    //     throw new Error("Should be equal to 13, Got " + total)
-    // }
-})
+// test('should calculate total with tip', () => {
+//     const total = calculateTip(10, .3)
+//     expect(total).toBe(13)
+//     // if (total !== 13) {
+//     //     throw new Error("Should be equal to 13, Got " + total)
+//     // }
+// })
 
-test('should calculate tip with default tip', () => {
-    const total = calculateTip(10)
-    expect(total).toBe(12.5)
-})
+// test('should calculate tip with default tip', () => {
+//     const total = calculateTip(10)
+//     expect(total).toBe(12.5)
+// })
 
-test('Should convert 32 F to 0 C', () => {
-    // fahrenheitToCelsius
-    const convert = fahrenheitToCelsius(32)
-    expect(convert).toBe(0)
-})
+// test('Should convert 32 F to 0 C', () => {
+//     // fahrenheitToCelsius
+//     const convert = fahrenheitToCelsius(32)
+//     expect(convert).toBe(0)
+// })
 
-test('Should convert 0 C to 32 F', () => {
-    //celsiusToFahrenheit
-    const convert = celsiusToFahrenheit(0)
-    expect(convert).toBe(32)
-})
+// test('Should convert 0 C to 32 F', () => {
+//     //celsiusToFahrenheit
+//     const convert = celsiusToFahrenheit(0)
+//     expect(convert).toBe(32)
+// })
 
 // test("Async test demo", (done) => {
 //     setTimeout(() => {
@@ -32,12 +32,12 @@ test('Should convert 0 C to 32 F', () => {
 //     }, 2000)
 // })
 
-test('should add two numbers', (done) => {
-    add(2, 4).then((sum) => {
-        expect(sum).toBe(6)
-        done()
-    })
-})
+// test('should add two numbers', (done) => {
+//     add(2, 4).then((sum) => {
+//         expect(sum).toBe(6)
+//         done()
+//     })
+// })
 
 test('should add two numbers async/await', async () => {
     const sum = await add(3, 17)
--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -1,11 +1,18 @@
 const request = require("supertest")
+const jwt = require("jsonwebtoken")
+const mongoose = require('mongoose')
 const app = require("../src/app")
 const User = require("../src/models/user")
 
+const userOneId = new mongoose.Types.ObjectId
 const userOne = {
+    _id: userOneId,
     name: "Mike",
     email: "mike@example.com",
-    password: 'aliali12'
+    password: 'aliali12',
+    tokens: [{
+        token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET)
+    }]
 }
 
 beforeEach(async () => {
@@ -38,3 +45,34 @@ test('should not login nonexisting user', async () => {
         password: "12121212"
     }).expect(400)
 })
+
+test('should get profile for user', async () => {
+    await request(app)
+        .get('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+})
+
+test('should not get profile for unauthenticated user', async () => {
+    await request(app)
+        .get('/users/me')
+        .send()
+        .expect(401)
+})
+
+test('should delete account for user', async () => {
+    await request(app)
+        .delete("/users/me")
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+})
+
+test('should not delete account for unauthenticated user', async () => {
+    await request(app)
+        .delete("/users/me")
+        .send()
+        .expect(401)
+})
+
