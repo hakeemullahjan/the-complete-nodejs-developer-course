cefebd2706dd4ef90aa0cbf3c3ccd30aa62a4b38
hakeemullahjan <h.jan210@gmail.com>
2020-04-13 11:45:50 +0500

    139 Writing Your Own Tests

--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -1,17 +1,30 @@
-const { calculateTip } = require("../src/math.js")
+const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit } = require("../src/math.js")
 
-test('should calculate total with tip', () => {
-    const total = calculateTip(10, .3)
+// test('should calculate total with tip', () => {
+//     const total = calculateTip(10, .3)
+//     expect(total).toBe(13)
+//     // if (total !== 13) {
+//     //     throw new Error("Should be equal to 13, Got " + total)
+//     // }
+// })
 
-    expect(total).toBe(13)
+// test('should calculate tip with default tip', () => {
+//     const total = calculateTip(10)
+//     expect(total).toBe(12.5)
+// })
 
-    // if (total !== 13) {
-    //     throw new Error("Should be equal to 13, Got " + total)
-    // }
+test('Should convert 32 F to 0 C', () => {
+    // fahrenheitToCelsius
+    const convert = fahrenheitToCelsius(32)
+    expect(convert).toBe(0)
 })
 
-test('should calculate tip with default tip', () => {
-    const total = calculateTip(10)
-    expect(total).toBe(12.5)
+test('Should convert 0 C to 32 F', () => {
+    //celsiusToFahrenheit
+    const convert = celsiusToFahrenheit(0)
+    expect(convert).toBe(32)
 })
+
+
+
 
