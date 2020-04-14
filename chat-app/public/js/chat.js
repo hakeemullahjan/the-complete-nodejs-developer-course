15a15b740662b5898601c2293432f326a387ba91
hakeemullahjan <h.jan210@gmail.com>
2020-04-14 20:20:13 +0500

    159 Event Acknowledgements

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -8,7 +8,13 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
     e.preventDefault()
     const messageText = e.target.elements.message.value
 
-    socket.emit("sendMessage", messageText)
+    socket.emit("sendMessage", messageText, (error) => {
+        if (error) {
+            return console.log(error)
+        }
+
+        console.log("Message delivered")
+    })
 
 })
 
@@ -20,7 +26,13 @@ document.querySelector("#send-location").addEventListener('click', () => {
 
     navigator.geolocation.getCurrentPosition((position) => {
         // console.log(position.coords)
-        socket.emit("sendLocation", { latitude: position.coords.latitude, longitude: position.coords.longitude })
+        socket.emit("sendLocation",
+            {
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude
+            }, () => {
+                console.log("Location shared!")
+            })
     })
 
 })
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -3,6 +3,7 @@ const path = require("path")
 const port = process.env.PORT || 3000
 const http = require("http")
 const socketio = require("socket.io")
+const Filter = require("bad-words")
 
 const app = express()
 const server = http.createServer(app)
@@ -11,7 +12,6 @@ const io = socketio(server)
 const publicDirectoryPath = path.join(__dirname, '../public')
 app.use(express.static(publicDirectoryPath))
 
-let count = 0
 io.on("connection", (socket) => {
     console.log("New WebSocket connection")
 
@@ -19,13 +19,19 @@ io.on("connection", (socket) => {
 
     socket.broadcast.emit('message', 'A new user has joined!')
 
-    socket.on("sendMessage", (message) => {
+    socket.on("sendMessage", (message, callback) => {
+        const filter = new Filter()
+        if (filter.isProfane(message)) {
+            return callback("Profanity is not allowed")
+        }
+
         io.emit("message", message)
+        callback()
     })
 
-
-    socket.on('sendLocation', (coords) => {
+    socket.on('sendLocation', (coords, callback) => {
         io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+        callback()
     })
 
     socket.on("disconnect", () => {
