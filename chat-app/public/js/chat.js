9f6716673e9220794f0bf8bc6de4f6cb19a30aa1
hakeemullahjan <h.jan210@gmail.com>
2020-04-14 12:32:20 +0500

    155 Socket.io Events

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,1 +1,10 @@
-io()
+const socket = io()
+
+socket.on("countUpdate", (count) => {
+    console.log("The count has been updated", count)
+})
+
+document.querySelector('#increment').addEventListener("click", () => {
+    console.log("Clicked")
+    socket.emit("increment")
+})
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -11,8 +11,18 @@ const io = socketio(server)
 const publicDirectoryPath = path.join(__dirname, '../public')
 app.use(express.static(publicDirectoryPath))
 
-io.on("connection", () => {
+let count = 0
+io.on("connection", (socket) => {
     console.log("New WebSocket connection")
+
+    socket.emit("countUpdate", count)
+
+    socket.on("increment", () => {
+        count++
+        // socket.emit("countUpdate", count)
+        io.emit("countUpdate", count)
+    })
+
 })
 
 server.listen(port, () => {
