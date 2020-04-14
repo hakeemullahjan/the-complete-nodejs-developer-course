62c22d1b9f581942e075ec0877ff83c37eb6ab7a
hakeemullahjan <h.jan210@gmail.com>
2020-04-14 19:55:02 +0500

    158 Sharing Your Location

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -11,3 +11,16 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
     socket.emit("sendMessage", messageText)
 
 })
+
+
+document.querySelector("#send-location").addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert("Your browser doesnot suppert geolocation")
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        // console.log(position.coords)
+        socket.emit("sendLocation", { latitude: position.coords.latitude, longitude: position.coords.longitude })
+    })
+
+})
