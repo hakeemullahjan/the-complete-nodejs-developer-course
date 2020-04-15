f9e9060aa24753782071adf17e75256053e534d3
hakeemullahjan <h.jan210@gmail.com>
2020-04-15 10:33:40 +0500

    160 Form and Button States

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,14 +1,29 @@
 const socket = io()
 
+// Elements
+const $messageForm = document.querySelector("#message-form")
+const $messageFormInput = $messageForm.querySelector("input")
+const $messageFormButton = $messageForm.querySelector("button")
+const $sendLocationButton = document.querySelector('#send-location')
+
 socket.on("message", (message) => {
     console.log(message)
 })
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
+
+    $messageFormButton.setAttribute("disabled", 'disabled')
+
+    //disabled
     const messageText = e.target.elements.message.value
 
     socket.emit("sendMessage", messageText, (error) => {
+        $messageFormButton.removeAttribute("disabled")
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
+        //enabled
         if (error) {
             return console.log(error)
         }
@@ -19,10 +34,13 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
 })
 
 
-document.querySelector("#send-location").addEventListener('click', () => {
+$sendLocationButton.addEventListener('click', () => {
     if (!navigator.geolocation) {
         return alert("Your browser doesnot suppert geolocation")
     }
+
+    //disabled
+    $sendLocationButton.setAttribute("disabled", 'disabled')
 
     navigator.geolocation.getCurrentPosition((position) => {
         // console.log(position.coords)
@@ -31,6 +49,8 @@ document.querySelector("#send-location").addEventListener('click', () => {
                 latitude: position.coords.latitude,
                 longitude: position.coords.longitude
             }, () => {
+                $sendLocationButton.removeAttribute("disabled")
+                //enabled
                 console.log("Location shared!")
             })
     })
